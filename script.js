ceae9511d86288080884e285110ff85883bab531
kekYaYlol <matveevivan337@gmail.com>
2024-11-16 15:40:35 +0200

    print clock for site

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function updateClock(){
-
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = now.getMinutes().padStart(2, "0");
+    const seconds = now.getSeconds().padStart(2, "0");
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
 
-document.getElementById('clock').textContent = new Date()
+setInterval(updateClock, 1000)
