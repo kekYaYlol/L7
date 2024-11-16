54582c210a8c1c93f3d4d9f7555417dc57e9a265
kekYaYlol <matveevivan337@gmail.com>
2024-11-16 16:41:44 +0200

    print clock for site

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function updateClock(){
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = now.getMinutes().padStart(2, "0");
-    const seconds = now.getSeconds().padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+    const selectTimeZona = document.getElementById('timeZona');
+    timeZona = parseInt(selectTimeZona.value, 10); 
+    // console.log(timeZona)
 }
 
-setInterval(updateClock, 1000)
+let timeZona = 0;
+
+// selectTimeZona.addEventListener('change', updateClock)
+setInterval(updateClock(timeZona), 1000)
+
+// const different_time_UTC = new Date().getTimezoneOffset() / 60;
+// const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZona;
+// const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZona;
+// document.getElementById('clock2').textContent = timeZona;
